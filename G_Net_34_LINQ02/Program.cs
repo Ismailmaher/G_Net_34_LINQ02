bd17c97f125e85cbe4b9b527469442c5268fae7a
Isamilmaher <ismailmaher6977@gmail.com>
2026-04-15 14:17:01 +0200

    Question 2

--- G_Net_34_LINQ02/Program.cs
+++ G_Net_34_LINQ02/Program.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using G_Net_34_LINQ02.DataSources;
+using G_Net_34_LINQ02.Helper;
+using G_Net_34_LINQ02.Models;
+using System.Collections;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Globalization;
 using System.Net.Http.Headers;
 using System.Security.Cryptography;
-using G_Net_34_LINQ02.Helper;
-using G_Net_34_LINQ02.Models;
 using static G_Net_34_LINQ02.DataSources.Source;
 
 namespace G_Net_34_LINQ02
@@ -24,6 +25,17 @@ namespace G_Net_34_LINQ02
             //foreach (var product in topthreeExpensiveProducts)
             //{
             //    Console.WriteLine($"{product.UnitPrice}");
+            //}
+            #endregion
+            #region Question 2
+            //show page 2 of products, with page size = 5
+            //var result = ProductList
+            //.Skip(5) 
+            //.Take(5)                 
+            //.ToList();
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine($"ID: {item.ProductID} | Product: {item.ProductName} | Price: {item.UnitPrice:C}");
             //}
             #endregion
         }
