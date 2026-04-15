e235a266a09c8e017578f76de1a5cb292d342b61
Isamilmaher <ismailmaher6977@gmail.com>
2026-04-15 14:04:43 +0200

    Question 1

--- G_Net_34_LINQ02/Program.cs
+++ G_Net_34_LINQ02/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace G_Net_34_LINQ02
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using G_Net_34_LINQ02.Helper;
+using G_Net_34_LINQ02.Models;
+using static G_Net_34_LINQ02.DataSources.Source;
+
+namespace G_Net_34_LINQ02
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+            //1.Get top 3 most expensive products
+            //var topthreeExpensiveProducts = ProductList
+            //    .OrderByDescending(p => p.UnitPrice)
+            //    .Take(3)
+            //    .ToList();
+            //foreach (var product in topthreeExpensiveProducts)
+            //{
+            //    Console.WriteLine($"{product.UnitPrice}");
+            //}
+            #endregion
         }
     }
 }
